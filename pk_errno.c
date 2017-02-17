c42e451d6673a0d1df01f9ae960a556f1419be29
xuzhiyuan <myjalo@163.com>
2017-02-17 21:40:55 +0800

    调整pk_error.h pk_error.c错误码、config.ini中为exist_led增加注释

--- pk_errno.c
+++ pk_errno.c
@@ -20,18 +20,21 @@ extern "C" {
 
 const char *pk_errlist[] =
     {
-    /*  0:PK_SUCCESS                  */  "No error",
-    /*  1:RFM2G_TIMED_OUT             */  "A wait timed out",
-    /*  2:RFM2G_OS_ERROR              */  "Function failed for other OS defined error",
-    /*  3:RFM2G_BAD_PARAMETER_1       */  "Function Parameter 1 in RFM2g API call is either NULL or invalid",
-    /*  4:RFM2G_BAD_PARAMETER_2       */  "Function Parameter 2 in RFM2g API call is either NULL or invalid",
-    /*  5:RFM2G_BAD_PARAMETER_3       */  "Function Parameter 3 in RFM2g API call is either NULL or invalid",
-    /*  6:RFM2G_BAD_PARAMETER_4       */  "Function Parameter 4 in RFM2g API call is either NULL or invalid",
-    /*  7:RFM2G_BAD_PARAMETER_5       */  "Function Parameter 5 in RFM2g API call is either NULL or invalid",
-    /*  8:RFM2G_BAD_PARAMETER_6       */  "Function Parameter 6 in RFM2g API call is either NULL or invalid",
-    /*  9:RFM2G_BAD_PARAMETER_7       */  "Function Parameter 7 in RFM2g API call is either NULL or invalid",
-    /* 10:RFM2G_BAD_PARAMETER_8       */  "Function Parameter 8 in RFM2g API call is either NULL or invalid",
-    /* 11:RFM2G_MAX_ERROR_CODE        */  "Invalid error code"
+    /*   0:PK_SUCCESS               */  "No error",
+    /*   1:PK_TIMED_OUT             */  "A wait timed out",
+    /*   2:PK_OS_ERROR              */  "Function failed for other OS defined error",
+    /*   3:PK_FILE_ERROR            */  "File error",
+    /*   4:PK_FILE_EMPTY            */  "File empty",
+    /*   5:PK_BAD_PARAMETER_1       */  "Function Parameter 1 is either NULL or invalid",
+    /*   6:PK_BAD_PARAMETER_2       */  "Function Parameter 2 is either NULL or invalid",
+    /*   7:PK_BAD_PARAMETER_3       */  "Function Parameter 3 is either NULL or invalid",
+    /*   8:PK_BAD_PARAMETER_4       */  "Function Parameter 4 is either NULL or invalid",
+    /*   9:PK_BAD_PARAMETER_5       */  "Function Parameter 5 is either NULL or invalid",
+    /*  10:PK_BAD_PARAMETER_6       */  "Function Parameter 6 is either NULL or invalid",
+    /*  11:PK_BAD_PARAMETER_7       */  "Function Parameter 7 is either NULL or invalid",
+    /*  12:PK_BAD_PARAMETER_8       */  "Function Parameter 8 is either NULL or invalid",
+    /*  13:PK_SEND_ERROR            */  "send operation failed",
+    /*  14:PK_MAX_ERROR_CODE        */  "Invalid error code"
     };
 
 const int pk_nerr = sizeof(pk_errlist) / sizeof(char*);
